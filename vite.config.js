7807deddd963d4e87a5de0697afb065f8d9c8616
Camilo-Correa <correafuentes.cristian@gmail.com>
2025-03-23 00:32:16 -0500

    Agregar dominio permitido en preview.allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true, // Permite conexiones externas
-    port: process.env.PORT || 3000, // Usa el puerto asignado por Render o 3000 como fallback
+    port: process.env.PORT || 3000, // Usa el puerto asignado o 3000 por defecto
+  },
+  preview: {
+    allowedHosts: ['web-mudanza.onrender.com'], // Agrega tu dominio aquí
   },
 });
