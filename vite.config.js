d30de07214f23fe881de7b1666f2721ce9277b96
Camilo-Correa <correafuentes.cristian@gmail.com>
2025-03-23 00:27:32 -0500

    Añadir configuración de servidor para Render y soporte React

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // Permite conexiones externas
+    port: process.env.PORT || 3000, // Usa el puerto asignado por Render o 3000 como fallback
+  },
+});
